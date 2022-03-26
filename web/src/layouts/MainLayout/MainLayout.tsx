357ab36a0f42e355fc5273705c893f0e2edd8274
Kris Coulson <kriscoulson@gmail.com>
2022-03-26 16:34:51 -0700

    Add hero section

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -13,7 +13,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
       <Toaster toastOptions={{ className: 'mt-16' }} />
       <Navbar />
 
-      <main className="flex-1 w-full bg-orange-50">{children}</main>
+      <main className="flex-1 w-full">{children}</main>
 
       <footer className="p-6 bg-orange-50 text-orange-700 text-sm border-t border-orange-200">
         Copyright &copy;{new Date().getFullYear()} Tom Preston-Werner
